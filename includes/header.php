5ed3b791d2457fbc69380c6a1f37be6c468542ef
QuentinMcCarthy <qmccarthy97@gmail.com>
2018-11-15 11:50:57 +1300

    Layout initialised, design idea in mind, working on main sidebar.

--- includes/header.php
+++ includes/header.php
@@ -4,8 +4,15 @@
 		<meta charset="utf-8">
 		<title>Quentin McCarthy's Portfolio</title>
 
+		<!-- Fonts -->
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Arimo">
+
+		<!-- FontAwesome -->
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+
 		<!-- Bootstrap -->
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
 		<link rel="stylesheet" href="css/master.css">
 	</head>
 	<body>
--- index.php
+++ index.php
@@ -1,7 +1,36 @@
-<?php require "./includes/head.php" ?>
+<?php require "./includes/header.php" ?>
+	<div class="container-fluid pt-5 pb-5">
+		<div class="row">
+			<div id="userSidebar" class="col-6 bg-dark text-light pt-2 pb-2">
+				<div class="row">
+					<div class="col d-flex align-items-center justify-content-between">
+						<img id="userImage" class="img-fluid" src="http://placehold.it/150x150" alt="User Portrait">
 
-<div class="container">
+						<h1>Quentin M<sup>c</sup>Carthy</h1>
+					</div>
+				</div>
 
-</div>
+				<div class="row">
+					<div class="col pl-3 pr-3 pt-3">
+						<p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+					</div>
+				</div>
 
+				<div class="row">
+					<div class="col">
+						<hr>
+						<a href="https://www.facebook.com/"><i class="fab fa-facebook-square fa-2x"></i></a>
+					</div>
+				</div>
+			</div>
+
+			<div class="col">
+				<div class="row">
+					<div class="col">
+						<h1>Grid</h1>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 <?php require "./includes/footer.php" ?>
