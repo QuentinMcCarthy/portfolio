13d7465a4e0d31c8853abced7c668aac48002fce
QuentinMcCarthy <qmccarthy97@gmail.com>
2018-11-21 11:37:58 +1300

    Updated grid

--- index.php
+++ index.php
@@ -33,6 +33,38 @@
 					<div class="col">
 						<h1>Grid</h1>
 					</div>
+					<div class="col">
+						<h1>Grid</h1>
+					</div>
+					<div class="col">
+						<h1>Grid</h1>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col">
+						<h1>Grid</h1>
+					</div>
+					<div class="col">
+						<h1>Grid</h1>
+					</div>
+					<div class="col">
+						<h1>Grid</h1>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col">
+						<h1>Grid</h1>
+					</div>
+					<div class="col">
+						<h1>Grid</h1>
+					</div>
+					<div class="col">
+						<h1>Grid</h1>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 				</div>
 			</div>
 		</div>
