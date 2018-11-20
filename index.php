38736a3828cc19b3ab8248c6faab7aa92c358fd6
QuentinMcCarthy <qmccarthy97@gmail.com>
2018-11-21 11:01:02 +1300

    Comments added for code readability

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php require "./includes/header.php" ?>
 	<div class="container-fluid pt-5 pb-5">
 		<div class="row">
+			<!-- Sidebar details for the user -->
 			<div id="userSidebar" class="col-6 bg-dark text-light pt-2 pb-2">
 				<div class="row">
 					<div class="col d-flex align-items-center justify-content-between">
@@ -10,12 +11,14 @@
 					</div>
 				</div>
 
+				<!-- Sidebar content -->
 				<div class="row">
 					<div class="col pl-3 pr-3 pt-3">
 						<p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 					</div>
 				</div>
 
+				<!-- Social media or other external links -->
 				<div class="row">
 					<div class="col">
 						<hr>
