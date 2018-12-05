c289fc14bd2ca46137412da12b8b8fce82342e7b
QuentinMcCarthy <qmccarthy97@gmail.com>
2018-12-06 10:48:47 +1300

    Fixed indentation

--- includes/projects.php
+++ includes/projects.php
@@ -14,16 +14,16 @@
 	<div class="row mx-0">
 		<?php foreach($allProjects as $project): ?>
 			<div class="project col-4 px-0" data-id="<?= $project['id']; ?>">
-					<div class="position-relative">
-						<img class="project-image" src="./uploads/projects/<?= $project['image']; ?>" alt="">
+				<div class="position-relative">
+					<img class="project-image" src="./uploads/projects/<?= $project['image']; ?>" alt="">
 
-						<a href="<?= $baseURL; ?>project/<?= $project['url']; ?>">
-							<div class="project-info position-absolute p-1">
-								<h2><?= $project['name']; ?></h2>
-								<p><?= $project["description"]; ?></p>
-							</div>
-						</a>
-					</div>
+					<a href="<?= $baseURL; ?>project/<?= $project['url']; ?>">
+						<div class="project-info position-absolute p-1">
+							<h2><?= $project['name']; ?></h2>
+							<p><?= $project["description"]; ?></p>
+						</div>
+					</a>
+				</div>
 			</div>
 		<?php endforeach; ?>
 	</div>
